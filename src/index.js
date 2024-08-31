225022137d9ec21870122410af991b172d232f42
Carlos0550 <carlospelinski05@gmail.com>
2024-08-31 12:16:55 -0300

    implementado una tarea programada para los vencimientos, endpoint para hacer entregas, eliminar clientes, etc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
+const cron = require("node-cron")
 const { v4: uuidv4 } = require('uuid');
 const clientSupabase = require("./DbConfiguration/supabaseClient.js")
 
@@ -13,33 +14,41 @@ app.get("/", (req,res)=>{
 })
 
 
-app.post("/create-client", async (req, res) => {
-    const { parsedValues } = req.body;
+let initDate = new Date().toISOString().split("T")[0]; 
+let endDate = new Date();
+endDate.setMonth(endDate.getMonth() + 1); 
+endDate = endDate.toISOString().split("T")[0];
 
-    let initDate = new Date().toISOString().split("T")[0]; 
-    let endDate = new Date();
-    endDate.setMonth(endDate.getMonth() + 1); 
-    endDate = endDate.toISOString().split("T")[0];
+app.post("/create-client", async (req, res) => {
+    const { parsedValues, valorCuota } = req.body;
+
 
     const uuidGenerated = uuidv4();
+    const dataQuery3 = {
+        entrega: valorCuota,
+        fecha_entrega: initDate
+    }
 
     const query1 = 'INSERT INTO clientes(id_cliente, datos_cliente) VALUES($1, $2)';
-    const query2 = 'INSERT INTO membresias(user_id, start_date, end_date, status) VALUES($1, $2, $3, $4)';
+    const query2 = 'INSERT INTO membresias(id_cliente, start_date, end_date, status) VALUES($1, $2, $3, $4)';
+    const query3 = 'INSERT INTO entregas(id_cliente,detalle_entrega) VALUES ($1, $2)'
 
     const arrayQuery1 = [uuidGenerated, parsedValues];
     const arrayQuery2 = [uuidGenerated, initDate, endDate, 'activa'];
+    const arrayQuery3 = [uuidGenerated, dataQuery3]
 
     if (parsedValues) {
         try {
             await clientSupabase.query("BEGIN");
             const response1 = await clientSupabase.query(query1, arrayQuery1);
             const response2 = await clientSupabase.query(query2, arrayQuery2);
+            const response3 = await clientSupabase.query(query3, arrayQuery3)
 
-            await clientSupabase.query("COMMIT");
-
-            if (response1.rowCount > 0 && response2.rowCount > 0) {
+            if (response1.rowCount > 0 && response2.rowCount > 0 && response3.rowCount > 0) {
+                await clientSupabase.query("COMMIT");
                 return res.status(200).json({ message: "Cliente y membresía guardados exitosamente" });
             } else {
+                await clientSupabase.query("ROLLBACK");
                 return res.status(406).json({ message: "Hubo un problema y no se pudo guardar el cliente o la membresía" });
             }
         } catch (error) {
@@ -106,6 +115,92 @@ app.get("/show-client-info", async (req, res) => {
         return res.status(500).json({ message: "Error interno del servidor, no se pudo mostrar los datos del cliente" });
     }
 });
+
+app.put("/update-data-client", async(req,res)=>{
+    const {values, userID} = req.body.values
+    const query = 'UPDATE clientes SET datos_cliente = $1 WHERE id_cliente = $2'
+    try {
+        const result = await clientSupabase.query(query, [JSON.stringify(values), userID])
+        if (result.rowCount > 0) {
+            return res.status(200).json({message: "Usuario actualizado!"})
+        }else{
+            return res.status(500).json({message: "Hubo un error al actualizar el cliente"})
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Error interno del servidor, no se pudo mostrar los datos del cliente" });
+    }
+});
+
+app.delete("/delete-client", async (req, res) => {
+    const { id_cliente } = req.query;
+    console.log(id_cliente);
+
+    const query1 = 'DELETE FROM clientes WHERE id_cliente = $1';
+    const query2 = 'DELETE FROM membresias WHERE id_cliente = $1';
+    const query3 = 'DELETE FROM entregas WHERE id_cliente = $1'
+
+    try {
+        await clientSupabase.query("BEGIN");
+
+        const response1 = await clientSupabase.query(query1, [id_cliente]);
+        const response2 = await clientSupabase.query(query2, [id_cliente]);
+        const response3 = await clientSupabase.query(query3, [id_cliente])
+
+        if (response1.rowCount > 0 || response2.rowCount > 0 || response3.rowCount > 0) {
+            await clientSupabase.query("COMMIT");
+            return res.status(200).json({ message: "Cliente eliminado exitosamente!" });
+        } else {
+            await clientSupabase.query("ROLLBACK");
+            return res.status(400).json({ message: "Hubo un error y no se pudo eliminar el cliente" });
+        }
+
+    } catch (error) {
+        await clientSupabase.query("ROLLBACK"); 
+        console.log(error);
+        return res.status(500).json({ message: "Error interno del servidor: no se pudo eliminar el cliente" });
+    }
+});
+
+
+cron.schedule('22 20 * * *', async () => {
+    console.log('Ejecutando tarea cron para actualizar estados de entregas...');
+    const query = "UPDATE membresias SET status = 'vencido' WHERE end_date <= CURRENT_DATE";
+    
+    try {
+        const result = await clientSupabase.query(query);
+        console.log(`Se actualizaron ${result.rowCount} filas a 'vencido'.`);
+    } catch (error) {
+        console.error('Error al ejecutar la tarea cron:', error);
+    }
+});
+
+app.post("/make-deliver", async(req,res)=>{
+    const {clientID, value, entrega_id} = req.body
+
+    
+    const serializedQuery1 = JSON.stringify([value])
+    const query1 = 'UPDATE entregas SET detalle_entrega = $1 WHERE id_cliente = $2'
+    const query2 = "UPDATE membresias SET status = 'activa', end_date = $1 WHERE id_cliente = $2"
+    
+    try {
+        await clientSupabase.query("BEGIN")
+        const response1 = await clientSupabase.query(query1, [serializedQuery1, clientID])
+        const response2 = await clientSupabase.query(query2, [endDate,clientID])
+
+        if (response1.rowCount > 0 || response2.rowCount > 0) {
+            await clientSupabase.query("COMMIT")
+            return res.status(200).json({message:"Membresia Actualizada!"})
+        }else{
+            await clientSupabase.query("ROLLBACK")
+            return res.status(400).json({message: "Entrega guardada y membresía actualizada!"})
+        }
+    } catch (error) {
+        await clientSupabase.query("ROLLBACK"); 
+        console.log(error);
+        return res.status(500).json({ message: "Error interno del servidor: guardar la entrega ni reiniciar la membresía" });
+    }
+})
 
 
 app.listen(port, ()=>{
